2e815ae35831bb38416d65a8652ae7c945882dc8
Ali Haydar <ali.haydar85@gmail.com>
2019-02-06 12:03:30 +1300

    Create a welcome page with basic style. Next step is to replace the welcome page by a map

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const WelcomeToRide = () => (
+    <div className="welcome">
+        <span className="welcome-title">Welcome to Ride Vis.</span>
+        <br />
+        <br />
+        Please update your ride's coordinates and see it presented on the map.
+    </div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<WelcomeToRide />, document.getElementById('root'));
